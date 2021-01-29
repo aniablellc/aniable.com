a35518a2a5f0334e036ff85f76a7954c0b70b13b
Sean O'Connor <seanoconnor941@gmail.com>
2021-01-28 22:15:43 -0700

    Initial code commit

---
+++ header.php
@@ -0,0 +1,70 @@
+<div class="logoline">
+  <p class="moby">
+    <a href="/"><img src="images/logomobius.gif" width="108" height="65" alt="revolving mobius strip logo" /></a>
+  </p>
+  <p class="yada">
+    <img src="images/aniable810blue.png" width="810" height="80" alt="Aniable - Bring Infinite Possibilities to Life" />
+  </p>
+  <p style="
+      position: absolute;
+      left: 140px;
+      top: 60px;
+      width: 160px;
+      height: 18px;
+    ">
+    <a href="/"><img src="images/aniablelink.png" alt="Aniable.com" /></a>
+  </p>
+</div>
+<!-- logoline -->
+
+<div id="menu">
+  <div class="wrapper_menu menu_light_theme">
+    <!-- BEGIN MENU WRAPPER -->
+    <ul class="menu menu_blue">
+      <!-- BEGIN MENU -->
+      <li class="nodrop"><a href="/">Home</a></li><!-- No Drop Down Item -->
+      <li class="left"><a href="#" class="drop">Resources</a><!-- Begin Contact Item -->
+        <div class="dropdown_1column ">
+          <!-- Begin 1 column container -->
+          <div class="col_1 firstcolumn">
+            <ul>
+              <li><a href="/"><span style="font-size: 1.3em;">Why Aniable?</span></a></li>
+              <li><a href="/"><span style="font-size: 1.3em;">Sign Up/Sign In</span></a></li>
+              <li><a href="/"><span style="font-size: 1.3em;">Create a Section</span></a></li>
+            </ul>
+          </div>
+        </div><!-- End 1 column container -->
+      </li><!-- End 1 column Item -->
+
+      <li class="right"><a href="#" class="drop">Help & Support</a><!-- Begin Contact Item -->
+        <div class="dropdown_2columns align_right">
+          <!-- Begin right aligned container -->
+          <p style="text-align: center;">Support for the <br><span style="font-size: 1.15em;"><b>Aniable Website</b></span><br>is provided though our centralized <br><span style="font-size: 1.15em;"><b>Aniable Support Ticket Center</b></span></P>
+          <ul>
+            <li style="width: 300px;"><a href="/"><span style="font-size: 1.3em;">Create a New Support Ticket</span></a></li>
+            <li style="width: 300px;"><a href="/"><span style="font-size: 1.3em;">Check Status of a Ticket</span></a></li>
+          </ul>
+
+
+
+        </div><!-- End right aligned container -->
+      </li><!-- End Contact Item -->
+
+
+      <li class="right"><a href="#" class="drop">Aniable Sites</a><!-- Begin Contact Item -->
+        <div class="dropdown_1column align_right">
+          <!-- Begin 1 column container -->
+          <div class="col_1 firstcolumn">
+            <ul>
+              <li><a href="http://www.aniable.com"><span style="font-size: 1.3em;">Aniable LLC</span></a></li>
+              <li><a href="http://www.aniablesoftware.com"><span style="font-size: 1.3em;">Aniable Software</span></a></li>
+              <li><a href="http://www.anilibre.com"><span style="font-size: 1.3em;">AniLibre</span></a></li>
+              <li><a href="/"><span style="font-size: 1.3em;">PrimeLock</span></a></li>
+            </ul>
+          </div>
+        </div><!-- End 1 column container -->
+      </li><!-- End 1 column Item -->
+    </ul><!-- END MENU -->
+  </div><!-- END MENU WRAPPER -->
+</div>
+<!-- menu -->
