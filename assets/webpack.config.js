7fc79b996c62ead5335436e65e68e902c9c70c2e
Sean O'Connor <seanoconnor941@gmail.com>
2021-04-17 18:01:34 -0600

    Setup Webpack and Babel

---
+++ assets/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: {
+    aniable: path.resolve(__dirname, 'js/index.js')
+  },
+  output: {
+    path: path.resolve(__dirname, '../'),
+    filename: '[name].js'
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          format: {
+            comments: false
+          }
+        },
+        extractComments: false
+      })
+    ]
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+
+      {
+        test: /\.s?[ca]ss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [new MiniCssExtractPlugin()]
+};
