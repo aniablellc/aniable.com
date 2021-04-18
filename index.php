93d8a9b34ffdbc0ae8aa5808cafc6527207fe3d4
Sean O'Connor <seanoconnor941@gmail.com>
2021-04-17 18:01:49 -0600

    Add index.php

--- index.php
+++ index.php
@@ -1,155 +1,46 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<?php
+
+/**
+ * Main code file for Aniable.com
+ *
+ * PHP Version: 8.0.1
+ *
+ * @category  Aniable
+ * @package   Default
+ * @author    Sean O'Connor <seano@aniable.com>
+ * @copyright 2021 Aniable, LLC
+ * @license   MIT https://mit-license.org/
+ * @link      https://github.com/aniablellc/aniable.com
+ */
+
+$title = "Aniable, LLC";
+$description = "Infinite Possibilities";
+$image = "https://placekitten.com/1200/630"; // Placeholder
+?>
+
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
-    <meta name="keywords" content="recording, audio-visual services," />
-    <meta name="description" content="aniable Home Page" />
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <title>Aniable LLC</title>
-    <link rel="canonical" href="http://www.aniable.com" />
-    <link href="./css/style.css" rel="stylesheet" type="text/css" media="screen" />
-    <link href="./menu/css/megamenu.css" rel="stylesheet" type="text/css" media="screen" />
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <!--
-        Animated Collapsible DIV v2.4- (c) Dynamic Drive DHTML code library (www.dynamicdrive.com)
-        Visit Dynamic Drive at http://www.dynamicdrive.com/ for this script and 100s more       
-    -->
-    <script type="text/javascript" src="./js/animatedcollapse.js"></script>
-    <script type="text/javascript">
-        animatedcollapse.addDiv('A', 'fade=1,speed=400,group=about,hide=1')
-        animatedcollapse.addDiv('B', 'fade=1,speed=400,group=about,hide=1')
-        animatedcollapse.addDiv('C', 'fade=1,speed=400,group=about,hide=1')
-        animatedcollapse.addDiv('D', 'fade=1,speed=400,group=about,hide=1')
-        animatedcollapse.addDiv('E', 'fade=1,speed=400,group=about,hide=1')
-        animatedcollapse.addDiv('F', 'fade=1,speed=400,group=about,hide=1')
-        animatedcollapse.addDiv('G', 'fade=1,speed=400,group=about,hide=1')
-        animatedcollapse.addDiv('H', 'fade=1,speed=400,group=about,hide=1')
-        animatedcollapse.addDiv('Z', 'fade=1,speed=400,group=about,hide=1')
-        animatedcollapse.init()
-    </script>
-    <script type="text/javascript" src="./menu/js/mobile.js"></script>
-    <script type="text/javascript" src="./menu/js/form.js"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title ?></title>
+    <meta name="description" content="<?php echo $description ?>">
+    <meta name="robots" content="index, follow">
+    <meta name="og:title" content="<?php echo $title ?>">
+    <meta name="og:description" content="<?php echo $description ?>">
+    <meta name="og:image" content="<?php echo $image ?>">
+    <link rel="stylesheet" href="aniable.css">
+    <script src="aniable.js" defer></script>
 </head>
 
 <body>
-
     <div id="wrapper">
-        <?php require "./header.php"; ?>
-        <div id="page">
-
-            <div id="headline">
-                <h1>aniable:<span style="font-size: 70%;"> - </span><span style="font-size: 60%;">able to be brought to
-                        life</span></h1>
-            </div>
-            <div id="contentfull">
-
-                <h2>About Aniable.com and Aniable LLC <span style="font-size: 75%;"></span></h2>
-
-                <div style="margin-left: 15px;">
-                    <a href="#" rel="toggle[E]" data-openimage="images/collapse.png"
-                        data-closedimage="images/expand.png"><img src="images/collapse.png" border="0" /><span
-                            style="font-size: 155%;"> What does aniable mean?</span></a>
-                    <div id="E" style="width: 875px; background: #DDDDDD; padding: 5px; font-size: 130%;">
-                        Aniable is a new word, you won't find it in any dictionary. Not yet, at least. I made it up. As
-                        an adjective it means "able to be brought to life." As a noun, it is the name of my company
-                        Aniable LLC.
-                    </div>
-                    <br>
-                    <a href="#" rel="toggle[F]" data-openimage="images/collapse.png"
-                        data-closedimage="images/expand.png"><img src="images/collapse.png" border="0" /><span
-                            style="font-size: 155%;"> What does Aniable LLC do?</span></a>
-                    <div id="F" style="width: 875px; background: #DDDDDD; padding: 5px; font-size: 130%;">
-                        That should be at least a little obvious from this "Front" page. We do software of various kinds
-                        including Windows applications - (see AniableSoftware.com below) Web applications and
-                        specialized web-sites (see AniLibre.com and PrimeLock.com below) and others to come yet (see
-                        "More to Come")
-                    </div>
-                    <br>
-                    <a href="#" rel="toggle[G]" data-openimage="images/collapse.png"
-                        data-closedimage="images/expand.png"><img src="images/collapse.png" border="0" /><span
-                            style="font-size: 155%;"> Who is Aniable LLC?</span></a>
-                    <div id="G" style="width: 875px; background: #DDDDDD; padding: 5px; font-size: 130%;">
-                        Mostly, it's just me. David Trump. No, I'm not related to that other "Trump". I'm "we" because
-                        Aniable LLC is a Limited Liability Company registered in the State of Wyoming. It consist of me
-                        and some cheerleaders: my wife, our three offspring (they're certainly not "children" any
-                        longer) and our six grand-children along with the kind help of a collection of wonderful
-                        friends.
-                    </div>
-                    <br>
-                    <a href="#" rel="toggle[H]" data-openimage="images/collapse.png"
-                        data-closedimage="images/expand.png"><img src="images/collapse.png" border="0" /><span
-                            style="font-size: 155%;"> What is Aniable.com for?</span></a>
-                    <div id="H" style="width: 875px; background: #DDDDDD; padding: 5px; font-size: 130%;">
-                        Aniable.com is the "Central Hub" of our Aniable LLC empire! (That's a joke folks.) Here we give
-                        you some information about our company and links to each of our product lines. In the future it
-                        will also allow you to centrally manage your Aniable account in one convenient place. (That's
-                        all part of the "More to Come" stuff and it's a big secret - but stay tuned - I think you'll
-                        enjoy it.)
-                    </div>
-                </div>
-                <h2>The Websites of Aniable LLC: <span style="font-size: 75%;"></span></h2>
-
-                <div style="margin-left: 15px;">
-                    <a href="http://www.aniablesoftware.com"><img src="images/AniSoft_thumb.jpg" /></a>&nbsp;&nbsp;Go
-                    to:&nbsp;<span style="font-size: 155%;"><a href="http://www.aniablesoftware.com">AniableSoftware.com
-                    </span></a><span style="font-size: 120%;"><i> (We publish Windows and Web software) </i></span>
-                    <a href="#" rel="toggle[A]" data-openimage="images/collapse.png"
-                        data-closedimage="images/expand.png"><img src="images/collapse.png" border="0" /></a>
-                    <div id="A" style="width: 875px; background: #DDDDDD; padding: 5px; font-size: 130%;">
-                        On our AniableSoftware site you will find our currently available Windows software offerings
-                        including some interesting free products. Click the AniableSoftware.com link to go to that site.
-                        It's in the beta stage right now but will be open for business soon.
-                    </div>
-                    <br><br>
-                    <a href="http://www.anilibre.com"><img src="images/AniLibre_thumb.jpg" /></a>&nbsp;&nbsp;Go
-                    to:&nbsp;<span style="font-size: 155%;"><a
-                            href="http://www.anilibre.com">AniLibre.com</span></a><span style="font-size: 120%;"><i> (We
-                            promote self-published writers and authors) </i></span>
-                    <a href="#" rel="toggle[B]" data-openimage="images/collapse.png"
-                        data-closedimage="images/expand.png"><img src="images/collapse.png" border="0" /></a>
-                    <div id="B" style="width: 875px; background: #DDDDDD; padding: 5px; font-size: 130%;">
-                        The publishing business is going through a period of big changes. It is returning more and more
-                        (and faster and faster) to the old "self publishing" model. Two hundred years ago most authors
-                        published their own work - the giant publishing houses that we've come to accept as "the norm"
-                        didn't, for the most part, exist.
-                        <br>AniLibre.com is a website where self-published authors can enhance and promote their work.
-                        It is not a sales site, but links to the services through which authors sell copies of their
-                        work. It serves both digital and print-on-demand items. Beyond that, however AniLibre provides a
-                        place where authors can present additional material to enhance their readers' experiences.
-                    </div>
-                    <br><br>
-                    <a href="http://www.primelock.net"><img src="images/PrimeLock_thumb.jpg" /></a>&nbsp;&nbsp;Go
-                    to:&nbsp;<span style="font-size: 155%;"><a
-                            href="http://www.primelock.net">PrimeLock.net</span></a><span style="font-size: 120%;"><i>
-                            (We provide software licensing tools for developers) </i></span>
-                    <a href="#" rel="toggle[C]" data-openimage="images/collapse.png"
-                        data-closedimage="images/expand.png"><img src="images/collapse.png" border="0" /></a>
-                    <div id="C" style="width: 875px; background: #DDDDDD; padding: 5px; font-size: 130%;">
-                        PrimeLock is a registration system for downloadable (try-it-before-you-buy-it) software. As a
-                        tool for software publishers it provides a secure and hacker resistent (nothing is completely
-                        hacker proof) method of generating unique registration codes for customers. For software buyers,
-                        it offers easy and almost fool-proof method of validating a newly purchased license. (nothing is
-                        completely fool-proof - it's foolish to think so)
-                    </div>
-                    <br><br><br><br>
-                    <img src="images/moretocome_thumb.png" /><span style="font-size: 155%;"> More to Come </span><span
-                        style="font-size: 120%;"><i> (We can't say any more right now - but there's more to come!)
-                        </i></span>
-                    <a href="#" rel="toggle[D]" data-openimage="images/collapse.png"
-                        data-closedimage="images/expand.png"><img src="images/collapse.png" border="0" /></a>
-                    <div id="D" style="width: 875px; background: #DDDDDD; padding: 5px; font-size: 130%;">
-                        We have big plans. They may or may not happen - soon or later - we're not sure. We have to
-                        finish getting these first three operations up and running before we spread ourselves too thin.
-                        So, stay tuned...
-                    </div>
-                    <br>
-                </div>
-            </div> <!-- page -->
-            <div id="tavfooter">
-                <p>Copyright &copy; 2010 - <?php echo date("Y", time() - 7200) ?>
-                    Aniable LLC, all rights reserved.</p>
-            </div> <!-- footer -->
-        </div> <!-- wrapper -->
+        <?php require './templates/header.php'; ?>
+        <main></main>
+        <?php require './templates/footer.php'; ?>
+    </div>
 </body>
 
 </html>
