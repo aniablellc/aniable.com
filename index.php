cc51d04b4e89862f37e09dd8884df3a45b5fa301
Sean O'Connor <seanoconnor941@gmail.com>
2021-04-20 17:42:25 -0600

    Add meta, manifest, and favicons

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
 $title = "Aniable, LLC";
 $description = "Infinite Possibilities";
 $image = "https://placekitten.com/1200/630"; // Placeholder
+$url = "https://aniable.com/";
 ?>
 
 <!DOCTYPE html>
@@ -23,24 +24,30 @@ $image = "https://placekitten.com/1200/630"; // Placeholder
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title ?></title>
+    <link rel="stylesheet" href="aniable.css">
     <meta name="description" content="<?php echo $description ?>">
-    <meta name="robots" content="index, follow">
     <meta name="og:title" content="<?php echo $title ?>">
     <meta name="og:description" content="<?php echo $description ?>">
     <meta name="og:image" content="<?php echo $image ?>">
-    <link rel="stylesheet" href="aniable.css">
-    <script src="aniable.js" defer></script>
+    <meta name="og:locale" content="en_US">
+    <meta name="og:type" content="website">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="og:url" content="<?php echo $url ?>">
+    <link rel="canonical" href="<?php echo $url ?>">
+    <link rel="icon" href="favicon.ico">
+    <link rel="icon" href="favicon.svg" type="image/svg+xml">
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <link rel="manifest" href="manifest.json">
+    <meta name="theme-color" content="#FFFFFF">
 </head>
 
 <body>
-    <div id="wrapper">
-        <?php require './templates/header.php'; ?>
-        <main></main>
-        <?php require './templates/footer.php'; ?>
-    </div>
+    <?php require './templates/header.php'; ?>
+    <main></main>
+    <?php require './templates/footer.php'; ?>
+    <script src="aniable.js"></script>
 </body>
 
 </html>
